f0a97659c51a4a8599ea317bcc88bdc0e5368443
Fadhil <fadhilsaheer@gmail.com>
2022-08-15 10:04:29 -0700

    react router setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,30 @@
 import React from 'react';
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
 
 export const App: React.FC = () => {
-  return <div></div>;
+  return (
+    <Router>
+      <h2>Common</h2>
+      <Routes>
+        <Route
+          path='/'
+          element={
+            <div>
+              <h2>home page</h2>
+              <Link to='/sample'>sample page</Link>
+            </div>
+          }
+        />
+        <Route
+          path='/sample'
+          element={
+            <div>
+              <h2>sample page</h2>
+              <Link to='/'>home page</Link>
+            </div>
+          }
+        />
+      </Routes>
+    </Router>
+  );
 };
