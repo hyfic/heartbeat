f78fa4d48d8777456e81cc8004c25ba1b11b5b8a
Fadhil <fadhilsaheer@gmail.com>
2022-08-17 07:18:32 -0700

    delete database button

---
+++ src/components/databaseSettings/DeleteButton.tsx
@@ -0,0 +1,117 @@
+import React, { useState } from 'react';
+import { DatabaseType } from '../../types/database';
+import { SetState } from '../../types/react';
+import { Trash } from 'tabler-icons-react';
+import {
+  Button,
+  Checkbox,
+  IconButton,
+  Modal,
+  ModalBody,
+  ModalCloseButton,
+  ModalContent,
+  ModalFooter,
+  ModalHeader,
+  ModalOverlay,
+  useDisclosure,
+  useToast,
+  Text,
+} from '@chakra-ui/react';
+import { deleteDatabaseHelper } from '../../api/database';
+
+interface Props {
+  db: DatabaseType;
+  setDatabases: SetState<DatabaseType[]>;
+}
+
+export const DeleteButton: React.FC<Props> = ({ db, setDatabases }) => {
+  const toast = useToast();
+  const { isOpen, onOpen, onClose } = useDisclosure();
+
+  const [loading, setLoading] = useState(false);
+  const [checked, setChecked] = useState(true);
+
+  const deleteDatabase = () => {
+    setLoading(true);
+
+    deleteDatabaseHelper(db.id, db.path, checked)
+      .then(() => {
+        setDatabases((prev: DatabaseType[]) => {
+          return prev.filter((database) => database.id !== db.id);
+        });
+        toast({
+          title: 'Deleted database successfully',
+          position: 'top-right',
+          isClosable: true,
+          duration: 3000,
+          status: 'success',
+        });
+        onClose();
+      })
+      .catch((err) => {
+        toast({
+          title: err,
+          description: 'Try running this application as administrator',
+          position: 'top-right',
+          isClosable: true,
+          duration: 3000,
+          status: 'error',
+        });
+      })
+      .finally(() => {
+        setLoading(false);
+      });
+  };
+
+  return (
+    <>
+      <IconButton
+        aria-label='Delete database'
+        colorScheme='red'
+        variant='ghost'
+        icon={<Trash strokeWidth={2} />}
+        onClick={onOpen}
+      />
+
+      <Modal isCentered isOpen={isOpen} onClose={onClose}>
+        <ModalOverlay />
+        <ModalContent>
+          <ModalHeader>Delete database</ModalHeader>
+          <ModalCloseButton />
+          <ModalBody>
+            <Text fontSize='lg'>
+              Are you sure you want to delete {db.name} ? This action can't be
+              undone.
+            </Text>
+            <Checkbox
+              size='lg'
+              mt={5}
+              isChecked={checked}
+              onChange={(e) => setChecked(e.target.checked)}
+            >
+              Delete database content
+            </Checkbox>
+          </ModalBody>
+
+          <ModalFooter>
+            <Button
+              disabled={loading}
+              variant='outline'
+              mr={3}
+              onClick={onClose}
+            >
+              Cancel
+            </Button>
+            <Button
+              colorScheme='red'
+              isLoading={loading}
+              onClick={deleteDatabase}
+            >
+              Delete database
+            </Button>
+          </ModalFooter>
+        </ModalContent>
+      </Modal>
+    </>
+  );
+};
