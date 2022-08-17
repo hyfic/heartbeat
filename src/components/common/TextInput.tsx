2bc70d42a0ff4b5033df2171e01033d7b1bafb25
Fadhil <fadhilsaheer@gmail.com>
2022-08-17 10:55:02 -0700

    common component

---
+++ src/components/common/TextInput.tsx
@@ -0,0 +1,49 @@
+import React from 'react';
+import { Input, Textarea } from '@chakra-ui/react';
+import { SetState } from '../../types/react';
+
+interface Props {
+  title: string;
+  value: string;
+  setValue: SetState<string>;
+  className?: string;
+  disabled?: boolean;
+  textArea?: boolean;
+  disableTitle?: boolean;
+}
+
+export const TextInput: React.FC<Props> = ({
+  title,
+  value,
+  setValue,
+  className,
+  disabled,
+  textArea,
+  disableTitle,
+}) => {
+  return (
+    <div className={className}>
+      {!disableTitle && <h2 className='mb-2 text-md'>{title}</h2>}
+      {textArea ? (
+        <Textarea
+          variant='filled'
+          placeholder={title}
+          value={value}
+          onChange={(e) => setValue(e.target.value)}
+          disabled={disabled}
+          size='lg'
+        />
+      ) : (
+        <Input
+          placeholder={title}
+          variant='filled'
+          size='lg'
+          value={value}
+          onChange={(e) => setValue(e.target.value)}
+          disabled={disabled}
+          autoComplete='off'
+        />
+      )}
+    </div>
+  );
+};
