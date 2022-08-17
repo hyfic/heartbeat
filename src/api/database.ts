be7970f1ec7840900a5ba6c169f93c4a5e6dcd94
Fadhil <fadhilsaheer@gmail.com>
2022-08-17 07:17:57 -0700

    database helpers

--- src/api/database.ts
+++ src/api/database.ts
@@ -3,20 +3,35 @@ import { invoke } from '@tauri-apps/api/tauri';
 // rust functions names
 const CREATE_DATABASE_FUNCTION = 'add_database';
 const READ_DATABASES_FUNCTION = 'read_databases';
+const EDIT_DATABASE_NAME_FUNCTION = 'update_database';
+const DELETE_DATABASE_FUNCTION = 'delete_database';
 
 export const createNewDatabaseHelper = (name: string, path: string) => {
-  return new Promise((resolve, reject) => {
-    invoke(CREATE_DATABASE_FUNCTION, {
-      path,
-      name,
-    })
-      .then(resolve)
-      .catch(reject);
+  return invoke(CREATE_DATABASE_FUNCTION, {
+    path,
+    name,
   });
 };
 
 export const readDatabasesHelper = () => {
-  return new Promise((resolve, reject) => {
-    invoke(READ_DATABASES_FUNCTION).then(resolve).catch(reject);
+  return invoke(READ_DATABASES_FUNCTION);
+};
+
+export const editDatabaseHelper = (id: number, name: string) => {
+  return invoke(EDIT_DATABASE_NAME_FUNCTION, {
+    id,
+    name,
+  });
+};
+
+export const deleteDatabaseHelper = (
+  id: number,
+  path: string,
+  deleteFile: boolean
+) => {
+  return invoke(DELETE_DATABASE_FUNCTION, {
+    id,
+    path,
+    deleteFile,
   });
 };
