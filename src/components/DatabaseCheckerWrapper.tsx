d711ad8a146b9e748375758e1a0437eada3e65a6
Fadhil <fadhilsaheer@gmail.com>
2022-08-17 12:04:54 -0700

    database checker

---
+++ src/components/DatabaseCheckerWrapper.tsx
@@ -0,0 +1,33 @@
+// This component check, if there is any database selected, if not it ask to create a database
+
+import { useContext } from 'react';
+import { ReactComponent } from '../types/react';
+import { Database } from 'tabler-icons-react';
+import { Button } from '@chakra-ui/react';
+import { CreateDatabaseWrapper } from './navbar/CreateDatabaseWrapper';
+import {
+  DatabaseContext,
+  DatabaseContextType,
+} from '../context/DatabaseContext';
+
+export const DatabaseCheckerWrapper: ReactComponent = ({ children }) => {
+  const { selectedDatabase } = useContext(
+    DatabaseContext
+  ) as DatabaseContextType;
+
+  return (
+    <div>
+      {!selectedDatabase && (
+        <div className='h-screen -mx-5 -my-3 w-full flex flex-col items-center justify-center'>
+          <h2 className='text-xl font-medium'>No database selected</h2>
+          <CreateDatabaseWrapper>
+            <Button size='lg' colorScheme='teal' mt={3} variant='outline'>
+              <Database className='mr-2' /> Create new database
+            </Button>
+          </CreateDatabaseWrapper>
+        </div>
+      )}
+      {selectedDatabase && children}
+    </div>
+  );
+};
