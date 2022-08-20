52742d3c7e4c3aff85da57426bd2d661162064e2
Fadhil <fadhilsaheer@gmail.com>
2022-08-20 15:43:49 +0530

    new record button

---
+++ src/components/patient/record/NewRecordButton.tsx
@@ -0,0 +1,179 @@
+import React, { useContext, useState } from 'react';
+import { RecordForm } from './RecordForm';
+import {
+  useDisclosure,
+  Button,
+  Drawer,
+  DrawerBody,
+  DrawerFooter,
+  DrawerHeader,
+  DrawerOverlay,
+  DrawerContent,
+  DrawerCloseButton,
+  useToast,
+  IconButton,
+} from '@chakra-ui/react';
+import { PatientDataType, PatientRecordType } from '../../../types/patient';
+import { SetState } from '../../../types/react';
+import { updatePatientHelper } from '../../../api/patient';
+import {
+  DatabaseContext,
+  DatabaseContextType,
+} from '../../../context/DatabaseContext';
+import {
+  PatientContext,
+  PatientContextType,
+} from '../../../context/PatientContext';
+import { Plus } from 'tabler-icons-react';
+
+interface Props {
+  patientData: PatientDataType;
+  setPatientData: SetState<PatientDataType>;
+  patientId: number;
+}
+
+export const NewRecordButton: React.FC<Props> = ({
+  patientId,
+  patientData,
+  setPatientData,
+}) => {
+  const { editPatient } = useContext(PatientContext) as PatientContextType;
+  const { selectedDatabase } = useContext(
+    DatabaseContext
+  ) as DatabaseContextType;
+
+  const { isOpen, onOpen, onClose } = useDisclosure();
+
+  const toast = useToast();
+
+  const btnRef = React.useRef<any>();
+
+  let lastMedicalRecord = patientData?.records
+    ? patientData.records.length != 0
+      ? patientData.records[0]
+      : undefined
+    : undefined;
+
+  const [record, setRecord] = useState<PatientRecordType>(
+    lastMedicalRecord
+      ? {
+          medicalBioData: {
+            height: lastMedicalRecord?.medicalBioData?.height || undefined,
+            weight: lastMedicalRecord?.medicalBioData?.weight || undefined,
+            bmi: lastMedicalRecord?.medicalBioData?.bmi || undefined,
+          },
+          examination: {
+            systemicExamination: {
+              diagnosis:
+                lastMedicalRecord?.examination?.systemicExamination?.diagnosis,
+            },
+          },
+        }
+      : {}
+  );
+  const [loading, setLoading] = useState(false);
+
+  const closeDrawer = () => {
+    setRecord({});
+    onClose();
+  };
+
+  const saveRecord = () => {
+    if (!selectedDatabase) return;
+
+    let patientRecords = patientData.records || [];
+
+    patientRecords.unshift({
+      ...record,
+      createdAt: Date.now(),
+    });
+
+    let patientUpdatedData: PatientDataType = {
+      ...patientData,
+      records: patientRecords,
+      updatedAt: Date.now(),
+    };
+
+    updatePatientHelper(
+      selectedDatabase?.path,
+      patientId,
+      JSON.stringify(patientUpdatedData)
+    )
+      .then(() => {
+        toast({
+          title: 'Added record successfully',
+          description: 'Latest changes in database',
+          duration: 3000,
+          isClosable: true,
+          position: 'top-right',
+          status: 'success',
+        });
+
+        editPatient(patientId, patientUpdatedData);
+
+        setPatientData(patientUpdatedData);
+        closeDrawer();
+      })
+      .catch((err) => {
+        toast({
+          title: err,
+          description: 'Please try again or report this as bug',
+          duration: 3000,
+          isClosable: true,
+          position: 'top-right',
+          status: 'error',
+        });
+      })
+      .finally(() => {
+        setLoading(false);
+      });
+  };
+
+  return (
+    <>
+      <IconButton
+        aria-label='New patient'
+        icon={<Plus size={18} />}
+        ml={2}
+        ref={btnRef}
+        onClick={onOpen}
+      />
+      <Drawer
+        isOpen={isOpen}
+        placement='right'
+        size='full'
+        onClose={closeDrawer}
+        finalFocusRef={btnRef}
+      >
+        <DrawerOverlay />
+        <DrawerContent>
+          <DrawerCloseButton />
+          <DrawerHeader>Create new record</DrawerHeader>
+
+          <DrawerBody>
+            <RecordForm
+              patientRecord={record}
+              setPatientRecord={setRecord}
+              loading={loading}
+            />
+          </DrawerBody>
+
+          <DrawerFooter>
+            <Button variant='outline' mr={3} onClick={closeDrawer}>
+              Cancel
+            </Button>
+            {/* <PreviewButton
+              patientCreatedAt={patientData.createdAt || 0}
+              patientBioData={patientData.bioData || {}}
+              patientRecord={record}
+              useButton
+            /> */}
+            <Button ml={3} colorScheme='blue' onClick={saveRecord}>
+              Save
+            </Button>
+          </DrawerFooter>
+        </DrawerContent>
+      </Drawer>
+    </>
+  );
+};
