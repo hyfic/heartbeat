11feb2a99e1b4925cec7778dd4d2323084d5dac7
Fadhil <fadhilsaheer@gmail.com>
2022-08-27 15:44:50 +0530

    added clickable table field to view patient

--- src/components/patientList/index.tsx
+++ src/components/patientList/index.tsx
@@ -2,10 +2,11 @@ import React, { useState } from 'react';
 import moment from 'moment';
 import { PatientDataType, PatientType } from '../../types/patient';
 import { TextInput } from '../common/TextInput';
-import { Link } from 'react-router-dom';
-import { ChevronDown, Folder, UserPlus } from 'tabler-icons-react';
+import { Link, useNavigate } from 'react-router-dom';
+import { ChevronDown, UserPlus } from 'tabler-icons-react';
 import { Paths } from '../../utils/paths';
 import { getId } from '../../utils/getId';
+import { OpenPatient } from './OpenPatient';
 import {
   Flex,
   IconButton,
@@ -23,14 +24,16 @@ import {
   MenuList,
   MenuItem,
 } from '@chakra-ui/react';
-import { OpenPatient } from './OpenPatient';
 
 interface Props {
   patients: PatientType[];
 }
 
 export const PatientList: React.FC<Props> = ({ patients }) => {
+  const navigate = useNavigate();
+
   const [searchQuery, setSearchQuery] = useState('');
+
   return (
     <div>
       <Flex mt={5} alignItems='center' justifyContent='space-between'>
@@ -69,8 +72,8 @@ export const PatientList: React.FC<Props> = ({ patients }) => {
               <Th>ID</Th>
               <Th>Avatar</Th>
               <Th>Name</Th>
+              <Th>Phone Number</Th>
               <Th>Last updated</Th>
-              <Th>View</Th>
             </Tr>
           </Thead>
           <Tbody>
@@ -91,24 +94,24 @@ export const PatientList: React.FC<Props> = ({ patients }) => {
                 let patient: PatientDataType = JSON.parse(patientJSON.data);
 
                 return (
-                  <Tr key={idx}>
+                  <Tr
+                    key={idx}
+                    className='cursor-pointer duration-200 transition-all hover:opacity-70'
+                    onClick={() =>
+                      navigate(
+                        `${Paths.patient}/${patientJSON.id}?path=${window.location.pathname}`,
+                        { replace: true }
+                      )
+                    }
+                  >
                     <Td>{idx + 1}</Td>
                     <Td>{getId(patient.createdAt || 0)}</Td>
                     <Td>
                       <Avatar name={patient.bioData?.name} size='sm' />
                     </Td>
                     <Td>{patient.bioData?.name}</Td>
+                    <Td>{patient.bioData?.phone || 'No phone'}</Td>
                     <Td>{moment(patient.updatedAt || 0).fromNow()}</Td>
-                    <Td>
-                      <Link to={`${Paths.patient}/${patientJSON.id}`} replace>
-                        <IconButton
-                          aria-label='View patient'
-                          icon={<Folder size={20} />}
-                          colorScheme='teal'
-                          variant='ghost'
-                        />
-                      </Link>
-                    </Td>
                   </Tr>
                 );
               })}
