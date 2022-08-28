0cdeab4dfb768e8b6b7e21d3c073d1f53e6ec5b8
Fadhil <fadhilsaheer@gmail.com>
2022-08-28 15:04:48 +0530

    only display appointed patients if a database is selected

--- src/components/navbar/index.tsx
+++ src/components/navbar/index.tsx
@@ -1,21 +1,28 @@
 import React, { useContext } from 'react';
 import { DatabaseOptions } from './DatabaseOptions';
+import { Navigation } from './Navigation';
+import { Paths } from '../../utils/paths';
+import { Badge, Flex, IconButton } from '@chakra-ui/react';
+import { Link } from 'react-router-dom';
 import {
   CalendarEvent,
   Settings,
   Snowman,
   Stethoscope,
 } from 'tabler-icons-react';
-import { Navigation } from './Navigation';
-import { Paths } from '../../utils/paths';
-import { Badge, Flex, IconButton } from '@chakra-ui/react';
-import { Link } from 'react-router-dom';
 import {
   PatientContext,
   PatientContextType,
 } from '../../context/PatientContext';
+import {
+  DatabaseContext,
+  DatabaseContextType,
+} from '../../context/DatabaseContext';
 
 export const Navbar: React.FC = () => {
+  const { selectedDatabase } = useContext(
+    DatabaseContext
+  ) as DatabaseContextType;
   const { appointedPatients } = useContext(
     PatientContext
   ) as PatientContextType;
@@ -29,7 +36,7 @@ export const Navbar: React.FC = () => {
         </Navigation>
         <Navigation page={Paths.appointments}>
           <CalendarEvent size={21} className='mr-1' /> Appointments
-          <Badge ml={1}>{appointedPatients.length}</Badge>
+          <Badge ml={1}>{selectedDatabase && appointedPatients.length}</Badge>
         </Navigation>
         <Navigation page={Paths.doctor}>
           <Stethoscope size={21} className='mr-1' /> Doctor
