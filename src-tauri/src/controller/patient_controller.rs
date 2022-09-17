eb9740501387533e3cf0c14a5fc5a49bd8a96e01
Fadhil <fadhilsaheer@gmail.com>
2022-09-17 19:12:54 +0530

    updated model & controller according to schema

--- src-tauri/src/controller/patient_controller.rs
+++ src-tauri/src/controller/patient_controller.rs
@@ -2,30 +2,46 @@ use crate::database::init_data_db;
 use crate::database::patient_model;
 
 #[tauri::command]
-pub fn create_patient(database_path: String, data: String) -> Result<(), String> {
+pub fn create_patient(
+    database_path: String,
+    pid: String,
+    created_at: String,
+    updated_at: String,
+    bio_data: String,
+    records: String,
+) -> Result<i64, String> {
     let db = init_data_db(database_path)?;
-    patient_model::create(&db, data)?;
-    Ok(())
+    let patient_id = patient_model::create(&db, pid, created_at, updated_at, bio_data, records)?;
+    Ok(patient_id)
 }
 
 #[tauri::command]
 pub fn read_patients(database_path: String) -> Result<Vec<patient_model::PatientType>, String> {
     let db = init_data_db(database_path)?;
-    let patients = patient_model::read_all(&db)?;
+    let patients = patient_model::read_all(&db, 1)?;
     Ok(patients)
 }
 
 #[tauri::command]
-pub fn read_patient(database_path: String, id: i32) -> Result<Vec<patient_model::PatientType>, String> {
+pub fn read_patient(
+    database_path: String,
+    id: i32,
+) -> Result<Vec<patient_model::PatientType>, String> {
     let db = init_data_db(database_path)?;
     let patients = patient_model::read_one(&db, id)?;
     Ok(patients)
 }
 
 #[tauri::command]
-pub fn update_patient(database_path: String, id: i32, data: String) -> Result<(), String> {
+pub fn update_patient(
+    database_path: String,
+    id: i32,
+    updated_at: String,
+    bio_data: String,
+    records: String,
+) -> Result<(), String> {
     let db = init_data_db(database_path)?;
-    patient_model::update(&db, id, data)?;
+    patient_model::update(&db, id, updated_at, bio_data, records)?;
     Ok(())
 }
 
--- src-tauri/src/database/patient_model.rs
+++ src-tauri/src/database/patient_model.rs
@@ -4,20 +4,42 @@ use serde::Serialize;
 #[derive(Serialize)]
 pub struct PatientType {
     pub id: i32,
-    pub data: String,
+    pub pid: String,
+    pub created_at: String,
+    pub updated_at: String,
+    pub bio_data: String,
+    pub records: String,
 }
 
-pub fn create(db: &Connection, data: String) -> Result<(), String> {
-    match db.execute("INSERT INTO Patient (data) VALUES (?1)", &[&data]) {
-        Ok(_) => return Ok(()),
+pub fn create(
+    db: &Connection,
+    pid: String,
+    created_at: String,
+    updated_at: String,
+    bio_data: String,
+    records: String,
+) -> Result<i64, String> {
+    match db.execute(
+        "INSERT INTO Patient (pid, created_at, updated_at, bio_data, records) VALUES (?1, ?2, ?3, ?4, ?5)",
+        &[&pid, &created_at, &updated_at, &bio_data, &records],
+    ) {
+        Ok(_) => {
+            let id = db.last_insert_rowid();
+            return Ok(id);
+        }
         Err(_) => return Err(String::from("Failed to save data")),
     }
 }
 
-pub fn read_all(db: &Connection) -> Result<Vec<PatientType>, String> {
+pub fn read_all(db: &Connection, page: i32) -> Result<Vec<PatientType>, String> {
     let mut patient_vec: Vec<PatientType> = Vec::new();
 
-    let mut sql_query = match db.prepare("SELECT * FROM Patient") {
+    let sql = format!(
+        "SELECT * FROM Patient LIMIT 10 OFFSET {} ORDER BY updated_at DESC",
+        page * 10
+    );
+
+    let mut sql_query = match db.prepare(&sql) {
         Ok(query) => query,
         Err(_) => return Err(String::from("Failed to load patients")),
     };
@@ -25,7 +47,11 @@ pub fn read_all(db: &Connection) -> Result<Vec<PatientType>, String> {
     let patient_iter = match sql_query.query_map([], |row| {
         Ok(PatientType {
             id: row.get(0)?,
-            data: row.get(1)?,
+            pid: row.get(1)?,
+            created_at: row.get(2)?,
+            updated_at: row.get(3)?,
+            bio_data: row.get(4)?,
+            records: row.get(5)?,
         })
     }) {
         Ok(patient_iter) => patient_iter,
@@ -38,6 +64,17 @@ pub fn read_all(db: &Connection) -> Result<Vec<PatientType>, String> {
             Err(_) => continue,
         }
     }
+
+    Ok(patient_vec)
+}
+
+pub fn search_patient(db: &Connection, search_query: String) -> Result<Vec<PatientType>, String> {
+    let mut patient_vec: Vec<PatientType> = Vec::new();
+
+    let sql = format!(
+        "SELECT * FROM Patient LIMIT 10 WHERE data LIKE %'\"name\":\"{}'%",
+        search_query
+    );
 
     Ok(patient_vec)
 }
@@ -54,7 +91,11 @@ pub fn read_one(db: &Connection, id: i32) -> Result<Vec<PatientType>, String> {
     let patient_iter = match sql_query.query_map(&[&id], |row| {
         Ok(PatientType {
             id: row.get(0)?,
-            data: row.get(1)?,
+            pid: row.get(1)?,
+            created_at: row.get(2)?,
+            updated_at: row.get(3)?,
+            bio_data: row.get(4)?,
+            records: row.get(5)?,
         })
     }) {
         Ok(patient_iter) => patient_iter,
@@ -71,10 +112,19 @@ pub fn read_one(db: &Connection, id: i32) -> Result<Vec<PatientType>, String> {
     Ok(patient_vec)
 }
 
-pub fn update(db: &Connection, id: i32, data: String) -> Result<(), String> {
+pub fn update(
+    db: &Connection,
+    id: i32,
+    updated_at: String,
+    bio_data: String,
+    records: String,
+) -> Result<(), String> {
     let id = format!("{}", id);
 
-    match db.execute("UPDATE Patient SET data=(?1) WHERE id=(?2)", &[&data, &id]) {
+    match db.execute(
+        "UPDATE Patient SET updated_at=(?1) bio_data=(?2) records=(?3) WHERE id=(?4)",
+        &[&updated_at, &bio_data, &records],
+    ) {
         Ok(_) => return Ok(()),
         Err(_) => return Err(String::from("Failed to update patient")),
     };
