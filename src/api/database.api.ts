33ba399771a873c2245a5c687f126bbad8f64e16
Fadhil <fadhilsaheer@gmail.com>
2022-10-09 11:02:13 +0530

    database api

---
+++ src/api/database.api.ts
@@ -0,0 +1,37 @@
+import { DatabaseType } from '@/types/database.type';
+import { invoke } from '@tauri-apps/api';
+
+// rust function names
+const READ_DATABASES = 'read_databases';
+const ADD_DATABASE = 'add_database';
+const UPDATE_DATABASE = 'update_database';
+const DELETE_DATABASE = 'delete_database';
+
+export const createNewDatabase = (path: string, name: string) => {
+  return invoke(ADD_DATABASE, { path, name });
+};
+
+export const readDatabases = () => {
+  return new Promise<DatabaseType[]>((resolve, reject) => {
+    invoke(READ_DATABASES).then(resolve).catch(reject);
+  });
+};
+
+export const updateDatabase = (id: number, name: string) => {
+  return invoke(UPDATE_DATABASE, {
+    id,
+    name,
+  });
+};
+
+export const deleteDatabase = (
+  id: number,
+  path: string,
+  deleteFile: boolean
+) => {
+  return invoke(DELETE_DATABASE, {
+    id,
+    path,
+    deleteFile,
+  });
+};
