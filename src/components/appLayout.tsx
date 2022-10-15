b24884f8b491da2d785b9cc8d8bb222570daa164
Fadhil <fadhilsaheer@gmail.com>
2022-10-15 20:06:15 +0530

    app layout

---
+++ src/components/appLayout.tsx
@@ -0,0 +1,22 @@
+import { ReactComponent } from '@/types/react.type';
+import { Flex, useColorMode } from '@chakra-ui/react';
+import { Navbar } from './navbar';
+
+export const AppLayout: ReactComponent = ({ children }) => {
+  const { colorMode } = useColorMode();
+
+  return (
+    <Flex>
+      <Flex
+        direction='column'
+        className='p-3 w-72 h-screen'
+        background={colorMode == 'dark' ? 'gray.700' : 'gray.50'}
+      >
+        <Navbar />
+      </Flex>
+      <div className='px-5 py-3 w-full h-screen overflow-y-scroll'>
+        {children}
+      </div>
+    </Flex>
+  );
+};
