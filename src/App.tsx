053a9f21f9f711569330cab636607dc2eefea580
Fadhil <fadhilsaheer@gmail.com>
2022-10-17 06:22:20 +0530

    implementation of react router dom

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,37 @@
 import React from 'react';
+import { AppLayout } from './components/appLayout';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { Paths } from '@/utils/paths';
+import { PatientListPage } from '@/pages/patientListPage';
+import { PatientPage } from '@/pages/patientPage';
+import { AppointedPatientsPage } from '@/pages/appointedPatientsPage';
+import { NewPatientPage } from '@/pages/newPatientPage';
+import { SettingsPage } from '@/pages/settingsPage';
+import { DatabaseSettingsPage } from '@/pages/databaseSettingsPage';
+import { DoctorPage } from '@/pages/doctorPage';
 
 export const App: React.FC = () => {
-  return <div>App</div>;
+  return (
+    <BrowserRouter>
+      <AppLayout>
+        <Routes>
+          <Route path={Paths.patientList} element={<PatientListPage />} />
+          <Route path={`${Paths.patient}/:id`} element={<PatientPage />} />
+          <Route
+            path={Paths.appointments}
+            element={<AppointedPatientsPage />}
+          />
+          <Route path={Paths.newPatient} element={<NewPatientPage />} />
+          <Route path={Paths.settings} element={<SettingsPage />} />
+          <Route
+            path={Paths.databaseSettings}
+            element={<DatabaseSettingsPage />}
+          />
+          <Route path={Paths.doctor} element={<DoctorPage />} />
+        </Routes>
+      </AppLayout>
+    </BrowserRouter>
+  );
 };
 
 export default App;
