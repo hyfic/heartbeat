df555f8a54aef7fcc3674c79bfea54f4cc3c2443
Fadhil <fadhilsaheer@gmail.com>
2022-10-19 04:12:28 +0530

    list header with search bar and option create patient

---
+++ src/components/patientList/listHeader.tsx
@@ -0,0 +1,50 @@
+import React, { useState } from 'react';
+import { Paths } from '@/utils/paths';
+import { ChevronDown, UserPlus } from 'tabler-icons-react';
+import { Link } from 'react-router-dom';
+import {
+  Button,
+  Flex,
+  IconButton,
+  Input,
+  Menu,
+  MenuButton,
+  MenuItem,
+  MenuList,
+} from '@chakra-ui/react';
+
+interface Props {
+  onSearch: (searchQuery: string) => void;
+}
+
+export const ListHeader: React.FC<Props> = ({ onSearch }) => {
+  const [searchQuery, setSearchQuery] = useState('');
+
+  return (
+    <Flex mt={5} alignItems='center' justifyContent='space-between'>
+      <Input
+        variant='filled'
+        placeholder='Search..'
+        onChange={(e) => onSearch(e.target.value)}
+      />
+      <Link to={Paths.newPatient} replace>
+        <Button ml={1}>
+          <UserPlus size={18} className='mr-1' /> New patient
+        </Button>
+      </Link>
+      <Menu>
+        <MenuButton
+          as={IconButton}
+          aria-label='Options'
+          icon={<ChevronDown size={18} />}
+          ml={1}
+        />
+        <MenuList>
+          <Link to={Paths.newPatient} replace>
+            <MenuItem icon={<UserPlus size={18} />}>New patient</MenuItem>
+          </Link>
+        </MenuList>
+      </Menu>
+    </Flex>
+  );
+};
