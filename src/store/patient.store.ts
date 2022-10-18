136de516874f107134b2f45f0cb07709f3a42c5c
Fadhil <fadhilsaheer@gmail.com>
2022-10-19 05:03:53 +0530

    search patient store

--- src/store/patient.store.ts
+++ src/store/patient.store.ts
@@ -1,6 +1,6 @@
 import create from 'zustand';
 import { PatientRawType } from '@/types/patient.type';
-import { readPatients } from '@/api/patient.api';
+import { readPatients, searchPatients } from '@/api/patient.api';
 
 interface PatientStore {
   patients: PatientRawType[];
@@ -15,6 +15,42 @@ export const usePatientStore = create<PatientStore>((set) => ({
   error: null,
   loadPatients(databasePath, page) {
     readPatients(databasePath, page)
+      .then((data) => {
+        set({
+          error: null, // to disable error if there was any previous errors
+          patients: data,
+          page, // tracking which page is loaded
+        });
+      })
+      .catch((err) => {
+        set({ error: err });
+      });
+  },
+}));
+
+// store to save patient search data
+interface PatientSearchStore {
+  patients: PatientRawType[];
+  page: number;
+  error: string | null;
+  searchPatients: (
+    databasePath: string,
+    searchQuery: string,
+    page: number
+  ) => void;
+}
+
+export const usePatientSearchStore = create<PatientSearchStore>((set) => ({
+  patients: [],
+  page: 1,
+  error: null,
+  searchPatients(databasePath, searchQuery, page) {
+    if (databasePath.trim().length === 0) {
+      set({ error: null, patients: [], page: 1 }); // resetting data if nothing is entered on search
+      return;
+    }
+
+    searchPatients(databasePath, searchQuery, page)
       .then((data) => {
         set({
           error: null, // to disable error if there was any previous errors
