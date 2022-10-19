e27c47aa1463e354e45960c6ad573d846248c86f
Fadhil <fadhilsaheer@gmail.com>
2022-10-19 05:39:43 +0530

    appointments page

--- src/pages/appointedPatientsPage.tsx
+++ src/pages/appointedPatientsPage.tsx
@@ -1,6 +1,46 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { DatabaseChecker } from '@/components/databaseChecker';
+import { useDatabaseStore } from '@/store/database.store';
+import { useAppointmentStore } from '@/store/appointment.store';
+import { PatientList } from '@/components/patientList';
+import { PatientBioDataType } from '@/types/patient.type';
 
 export const AppointedPatientsPage: React.FC = () => {
-  return <DatabaseChecker>Appointed patients page</DatabaseChecker>;
+  const { selectedDatabase } = useDatabaseStore();
+  const { appointments, loading, error, page, loadAppointments } =
+    useAppointmentStore();
+
+  const [searchQuery, setSearchQuery] = useState('');
+
+  const loadListPage = (page: number) => {
+    if (!selectedDatabase) return;
+    loadAppointments(selectedDatabase.path, page);
+  };
+
+  useEffect(() => {
+    loadListPage(1);
+  }, []);
+
+  return (
+    <DatabaseChecker>
+      <PatientList
+        title='Appointments'
+        patients={appointments.filter((patient) => {
+          let patientBioData: PatientBioDataType = JSON.parse(patient.bio_data);
+          return (
+            patientBioData.name
+              .trim()
+              .toLowerCase()
+              .includes(searchQuery.trim().toLowerCase()) ||
+            patient.pid.includes(searchQuery.trim())
+          );
+        })}
+        error={error}
+        loading={loading}
+        page={page}
+        onSearch={setSearchQuery}
+        loadListPage={loadListPage}
+      />
+    </DatabaseChecker>
+  );
 };
