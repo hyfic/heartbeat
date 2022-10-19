1a7ac22e88aa1227357b3fe9e0982858ea6d46ba
Fadhil <fadhilsaheer@gmail.com>
2022-10-19 05:31:26 +0530

    display spinner on loading

--- src/pages/patientListPage.tsx
+++ src/pages/patientListPage.tsx
@@ -6,11 +6,12 @@ import { DatabaseChecker } from '@/components/databaseChecker';
 
 export const PatientListPage: React.FC = () => {
   const { selectedDatabase } = useDatabaseStore();
-  const { patients, loadPatients, error, page } = usePatientStore();
+  const { patients, loadPatients, error, loading, page } = usePatientStore();
   const {
     patients: searchedPatients,
     searchPatients,
     error: searchError,
+    loading: searchLoading,
     page: searchPage,
   } = usePatientSearchStore();
 
@@ -44,6 +45,7 @@ export const PatientListPage: React.FC = () => {
         title='Patients'
         patients={searchQuery.trim().length === 0 ? patients : searchedPatients} // display searched patients if search query is provided, and vice versa
         error={error || searchError}
+        loading={loading || searchLoading}
         page={searchQuery.trim().length === 0 ? page : searchPage} // pass patient list page if there is nothing to search
         onSearch={handleSearch}
         loadListPage={loadListPage}
