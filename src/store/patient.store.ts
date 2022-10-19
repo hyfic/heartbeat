3435bed7deea71104e59b29b376855f13b697736
Fadhil <fadhilsaheer@gmail.com>
2022-10-19 05:31:15 +0530

    store loading state

--- src/store/patient.store.ts
+++ src/store/patient.store.ts
@@ -6,6 +6,7 @@ interface PatientStore {
   patients: PatientRawType[];
   page: number;
   error: string | null;
+  loading: boolean;
   loadPatients: (databasePath: string, page: number) => void;
 }
 
@@ -13,17 +14,20 @@ export const usePatientStore = create<PatientStore>((set) => ({
   patients: [],
   page: 1,
   error: null,
+  loading: false,
   loadPatients(databasePath, page) {
+    set({ loading: true });
     readPatients(databasePath, page)
       .then((data) => {
         set({
           error: null, // to disable error if there was any previous errors
           patients: data,
           page, // tracking which page is loaded
+          loading: false,
         });
       })
       .catch((err) => {
-        set({ error: err });
+        set({ error: err, loading: false });
       });
   },
 }));
@@ -33,6 +37,7 @@ interface PatientSearchStore {
   patients: PatientRawType[];
   page: number;
   error: string | null;
+  loading: boolean;
   searchPatients: (
     databasePath: string,
     searchQuery: string,
@@ -44,9 +49,12 @@ export const usePatientSearchStore = create<PatientSearchStore>((set) => ({
   patients: [],
   page: 1,
   error: null,
+  loading: false,
   searchPatients(databasePath, searchQuery, page) {
+    set({ loading: true });
+
     if (databasePath.trim().length === 0) {
-      set({ error: null, patients: [], page: 1 }); // resetting data if nothing is entered on search
+      set({ error: null, patients: [], page: 1, loading: false }); // resetting data if nothing is entered on search
       return;
     }
 
@@ -56,10 +64,11 @@ export const usePatientSearchStore = create<PatientSearchStore>((set) => ({
           error: null, // to disable error if there was any previous errors
           patients: data,
           page, // tracking which page is loaded
+          loading: false,
         });
       })
       .catch((err) => {
-        set({ error: err });
+        set({ error: err, loading: false });
       });
   },
 }));
