7d313c27a2d4fa3526c1fb68c47913b7b1df843f
Fadhil <fadhilsaheer@gmail.com>
2022-10-24 20:53:30 +0530

    save record

--- src/store/record.store.ts
+++ src/store/record.store.ts
@@ -1,21 +1,46 @@
 // to store currently editing record data
 
 import create from 'zustand';
-import { PatientRecordType } from '@/types/patient.type';
+import { PatientRecordType, PatientType } from '@/types/patient.type';
 import { defaultRecordData } from '@/utils/record';
+import { updatePatient } from '@/api/patient.api';
+import { showToast } from '@/utils/showToast';
 
 interface RecordStore {
   record: PatientRecordType;
-  nextAppointment: string;
   loading: boolean;
   setRecord: (record: PatientRecordType) => void;
-  setNextAppointment: (nextAppointment: string) => void;
+  saveRecord: (
+    databasePath: string,
+    patientData: PatientType,
+    onSuccess: () => void
+  ) => void;
 }
 
 export const useRecordStore = create<RecordStore>((set) => ({
   record: defaultRecordData,
-  nextAppointment: '',
   loading: false,
   setRecord: (record) => set({ record }),
-  setNextAppointment: (nextAppointment) => set({ nextAppointment }),
+  saveRecord(databasePath, patientData, onSuccess) {
+    set({ loading: true });
+    updatePatient(databasePath, patientData)
+      .then(() => {
+        showToast({
+          title: 'Added record successfully',
+          description: 'Latest changes in database',
+          status: 'success',
+        });
+        onSuccess();
+      })
+      .catch((err) => {
+        showToast({
+          title: err,
+          description: 'Please try again or report this as bug',
+          status: 'error',
+        });
+      })
+      .finally(() => {
+        set({ loading: false });
+      });
+  },
 }));
