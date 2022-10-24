7ca1d5fc167f1350bbb573ad379c72c05dd5762a
Fadhil <fadhilsaheer@gmail.com>
2022-10-24 19:25:00 +0530

    advice medicines

---
+++ src/components/patient/recordForm/adviceMedicines.tsx
@@ -0,0 +1,173 @@
+import React, { useState } from 'react';
+import { useRecordStore } from '@/store/record.store';
+import {
+  Flex,
+  IconButton,
+  Select,
+  Table,
+  TableContainer,
+  Tbody,
+  Td,
+  Th,
+  Thead,
+  Tr,
+} from '@chakra-ui/react';
+import { Plus, Trash } from 'tabler-icons-react';
+import { TextInput } from '@/components/common/textInput';
+
+export const AdviceMedicines: React.FC = () => {
+  const { loading, record, setRecord } = useRecordStore();
+
+  const [medicineName, setMedicineName] = useState('');
+  const [frequency, setFrequency] = useState('');
+  const [time, setTime] = useState('After food');
+  const [duration, setDuration] = useState('');
+  const [durationType, setDurationType] = useState('Day');
+
+  const addMedicine = (e: any) => {
+    e.preventDefault();
+
+    let medicineDuration = `${duration} ${durationType}${
+      Number(duration) != 1 ? 's' : ''
+    }`;
+
+    setRecord({
+      ...record,
+      medicines: [
+        ...(record.medicines || []),
+        {
+          medicineName,
+          frequency,
+          time,
+          duration: medicineDuration,
+        },
+      ],
+    });
+
+    setMedicineName('');
+    setFrequency('');
+    setTime('After food');
+    setDuration('');
+    setDurationType('Day');
+  };
+
+  return (
+    <TableContainer mt={5}>
+      <Table variant='simple'>
+        <Thead>
+          <Tr>
+            <Th>Index</Th>
+            <Th>Medicine</Th>
+            <Th>Frequency</Th>
+            <Th>Time</Th>
+            <Th>Duration</Th>
+            <Th>Option</Th>
+          </Tr>
+        </Thead>
+        <Tbody>
+          <>
+            {record.medicines &&
+              record.medicines.map((medicine, idx) => (
+                <Tr key={idx}>
+                  <Td>{idx + 1}</Td>
+                  <Td>{medicine.medicineName}</Td>
+                  <Td>{medicine.frequency}</Td>
+                  <Td>{medicine.time}</Td>
+                  <Td>{medicine.duration}</Td>
+                  <Td>
+                    <IconButton
+                      colorScheme='red'
+                      variant='ghost'
+                      aria-label='Delete medicine'
+                      icon={<Trash size={18} strokeWidth={2} />}
+                      onClick={() => {
+                        if (!record.medicines) return;
+                        let medicines = record.medicines;
+                        medicines.splice(idx, 1);
+                        setRecord({
+                          ...record,
+                          medicines,
+                        });
+                      }}
+                      disabled={loading}
+                    />
+                  </Td>
+                </Tr>
+              ))}
+            <Tr>
+              <Td>{(record.medicines?.length || 0) + 1}</Td>
+              <Td>
+                <TextInput
+                  disableTitle
+                  title='Medicine'
+                  value={medicineName}
+                  setValue={setMedicineName}
+                  disabled={loading}
+                />
+              </Td>
+              <Td>
+                <TextInput
+                  disableTitle
+                  title='Frequency'
+                  value={frequency}
+                  setValue={setFrequency}
+                  disabled={loading}
+                />
+              </Td>
+              <Td>
+                <Select
+                  variant='filled'
+                  value={time}
+                  onChange={(e) => setTime(e.target.value)}
+                >
+                  <option value='Before food'>Before food</option>
+                  <option value='Between food'>With food</option>
+                  <option value='After food'>After food</option>
+                </Select>
+              </Td>
+              <Td>
+                <Flex alignItems='center' justifyContent='space-around'>
+                  <TextInput
+                    disableTitle
+                    title='Duration'
+                    value={duration}
+                    setValue={setDuration}
+                    disabled={loading}
+                  />
+                  <Select
+                    variant='filled'
+                    value={durationType}
+                    onChange={(e) => setDurationType(e.target.value)}
+                    ml={2}
+                    width='38%'
+                  >
+                    <option value='Day'>D</option>
+                    <option value='Week'>W</option>
+                    <option value='Month'>M</option>
+                  </Select>
+                </Flex>
+              </Td>
+              <Td>
+                <IconButton
+                  onClick={addMedicine}
+                  variant='ghost'
+                  aria-label='Add medicine'
+                  colorScheme='blue'
+                  icon={<Plus />}
+                  disabled={
+                    loading ||
+                    medicineName.trim().length === 0 ||
+                    frequency.trim().length === 0 ||
+                    duration.trim().length === 0
+                  }
+                >
+                  Add Medicine
+                </IconButton>
+              </Td>
+            </Tr>
+          </>
+        </Tbody>
+      </Table>
+    </TableContainer>
+  );
+};
