14e0ec6d29a01dab663981efcedc84e9d9e1c90c
Fadhil <fadhilsaheer@gmail.com>
2022-10-25 20:45:06 +0530

    returned time in milli of dates

--- src/utils/time.ts
+++ src/utils/time.ts
@@ -1,9 +1,11 @@
 import moment from 'moment';
 
 export const getToday = () => {
-  return new Date(moment().format('YYYY-MM-DD')).toString();
+  return new Date(moment().format('YYYY-MM-DD')).getTime().toString();
 };
 
 export const getTomorrow = () => {
-  return new Date(moment().add(1, 'day').format('YYYY-MM-DD')).toString();
+  return new Date(moment().add(1, 'day').format('YYYY-MM-DD'))
+    .getTime()
+    .toString();
 };
