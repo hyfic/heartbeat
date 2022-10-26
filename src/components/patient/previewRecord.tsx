e090d0ea42d382e9bc8951ee936570dc2452877b
Fadhil <fadhilsaheer@gmail.com>
2022-10-26 09:29:35 +0530

    preview content

---
+++ src/components/patient/previewRecord.tsx
@@ -0,0 +1,56 @@
+import React from 'react';
+import { Eye } from 'tabler-icons-react';
+import { PreviewContent } from '../preview';
+import { useRecordStore } from '@/store/record.store';
+import { PatientRecordType } from '@/types/patient.type';
+import {
+  Button,
+  IconButton,
+  Modal,
+  ModalBody,
+  ModalContent,
+  ModalOverlay,
+  useDisclosure,
+} from '@chakra-ui/react';
+
+interface Props {
+  record: PatientRecordType;
+  useButton?: boolean;
+}
+
+export const PreviewRecord: React.FC<Props> = ({ useButton, record }) => {
+  const { isOpen, onOpen, onClose } = useDisclosure();
+  const { setRecord } = useRecordStore();
+
+  const handleOpenRecordPreview = () => {
+    onOpen();
+    setRecord(record);
+  };
+
+  return (
+    <>
+      {useButton ? (
+        <Button colorScheme='teal' onClick={handleOpenRecordPreview}>
+          Preview
+        </Button>
+      ) : (
+        <IconButton
+          aria-label='preview'
+          variant='ghost'
+          colorScheme='teal'
+          icon={<Eye size={18} />}
+          onClick={handleOpenRecordPreview}
+        />
+      )}
+      <Modal size='full' isOpen={isOpen} onClose={onClose}>
+        <ModalOverlay />
+        <ModalContent>
+          <ModalBody>
+            <div className='mt-5' />
+            <PreviewContent onClose={onClose} />
+          </ModalBody>
+        </ModalContent>
+      </Modal>
+    </>
+  );
+};
